cc5d78fc2efe86b227a334307377e5e3c2336bea
Ratnesh Ramesh Joshi <rrjoshi9057@gmail.com>
2026-05-01 04:06:53 +0530

    Added User Entity

---
+++ src/main/java/com/ratnesh/taskmanager/entity/Role.java
@@ -0,0 +1,7 @@
+package com.ratnesh.taskmanager.entity;
+
+public enum Role {
+    ADMIN,
+    MANAGER,
+    EMPLOYEE
+}
--- src/main/java/com/ratnesh/taskmanager/entity/User.java
+++ src/main/java/com/ratnesh/taskmanager/entity/User.java
@@ -1,4 +1,27 @@
 package com.ratnesh.taskmanager.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
