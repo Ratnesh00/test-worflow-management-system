d4f38c7aaaa2bc927b4923d9ac6d564f0d3c24b0
Ratnesh Ramesh Joshi <rrjoshi9057@gmail.com>
2026-05-25 01:23:51 +0530

    Added AuthService and password encryption

--- src/main/java/com/ratnesh/taskmanager/service/AuthService.java
+++ src/main/java/com/ratnesh/taskmanager/service/AuthService.java
@@ -1,4 +1,32 @@
 package com.ratnesh.taskmanager.service;
 
+import com.ratnesh.taskmanager.entity.User;
+import com.ratnesh.taskmanager.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passEncoder;
+    }
+
+    public User registerUser(User user) {
+
+        // Check if email already exists
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already registered");
+        }
+
+        // Encrypt password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        // Save user
+        return userRepository.save(user);
+    }
 }
